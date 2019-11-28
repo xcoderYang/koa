db9b118d0c463e77c91b513941b5c7d3347bc020
TS.SZX.xcyang <xcyang@travelsky.com>
2019-11-28 20:56:59 +0800

    update

--- static/config/webpack.config/node.js
+++
@@ -1,5 +0,0 @@
-process.argv.forEach((val, index)=>{
-  console.log(`${index}: ${val}`);
-})
-console.log(__dirname);
-console.log(__filename);
--- static/config/webpack.config/webpack.base.config.js
+++ static/config/webpack.config/webpack.base.config.js
@@ -72,28 +72,6 @@ module.exports = {
       }
     ]
   },
-  // optimization: {
-  //   splitChunks: {
-  //     chunks: "all",
-  //     minSize: 30000,
-  //     minChunks: 1,
-  //     maxAsyncRequests: 5,
-  //     maxInitialRequests: 3,
-  //     automaticNameDelimiter: '~',
-  //     name: true,
-  //     cacheGroups: {
-  //         vendors: {
-  //             test: /[\\/]node_modules[\\/]/,
-  //             priority: -10,
-  //         },
-  //     default: {
-  //             minChunks: 1,
-  //             priority: -20,
-  //             reuseExistingChunk: true,
-  //         }
-  //     }
-  //   }
-  // },
   plugins: [
     new VueLoaderPlugin(),
     new WriteFilePlugin(),
